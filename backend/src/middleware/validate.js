8839f83d80aecd69e035215654a7c3ba83f0a39c
mdrayhn3331-art <mdrayhn3331@gmail.com>
2026-08-27 05:23:21 +0600

    Create validate.js

---
+++ backend/src/middleware/validate.js
@@ -0,0 +1,42 @@
+const Joi = require('joi');
+
+exports.validateRegister = (req, res, next) => {
+  const schema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required()
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ success: false, message: error.details[0].message });
+  }
+  next();
+};
+
+exports.validateLogin = (req, res, next) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ success: false, message: error.details[0].message });
+  }
+  next();
+};
+
+exports.validateBet = (req, res, next) => {
+  const schema = Joi.object({
+    amount: Joi.number().min(1).required(),
+    clientSeed: Joi.string().required(),
+    prediction: Joi.any()
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ success: false, message: error.details[0].message });
+  }
+  next();
+};
