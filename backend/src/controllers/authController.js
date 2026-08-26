789cd2cbd43e631d39f61c942c10a00d1789e25c
mdrayhn3331-art <mdrayhn3331@gmail.com>
2026-08-27 05:24:18 +0600

    Create authController.js

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  });
+};
+
+exports.register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const existingUser = await User.findByEmail(email);
+    if (existingUser) {
+      return res.status(400).json
